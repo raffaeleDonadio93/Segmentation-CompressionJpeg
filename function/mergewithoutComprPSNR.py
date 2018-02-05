a1c93dfd8b49c493e1006da6f2b496af4cb4f35e
Giuseppe Capaldo <giuseppecapaldo93@gmail.com>
2018-02-05 12:03:01 +0100

    update

--- function/mergewithoutComprPSNR.py
+++ function/mergewithoutComprPSNR.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 from function.UnioneFrammenti import merge
 
 def mergewithoutCompression(filenameIntoImgInput,pathResult='RESULT/merge/withoutCompression/result.bmp'):
-    merge(pathResult,dirMerge="Frammenti",firstFragmentName='frammento0.bmp')
+    merge(pathResult,dirMerge="Frammenti")
     input = mpimg.imread('ImgInput\\' + filenameIntoImgInput)
     merged = mpimg.imread(pathResult)
     psnr1 = sky.compare_psnr(input, merged)
